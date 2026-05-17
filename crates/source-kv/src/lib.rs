65baaf6c2a414e32610d36543df145db1f84e8fe
laVashik <me@lavashik.dev>
2026-05-17 11:39:24 +0300

    feat(`source-kv`): added `ValueSerializer`

--- crates/source-kv/src/lib.rs
+++ crates/source-kv/src/lib.rs
@@ -1,7 +1,9 @@
 pub mod error;
 pub mod ser;
 pub mod de;
+pub mod value_ser;
 
 pub use error::{Error, Result};
 pub use ser::{to_string, Serializer};
 pub use de::{from_str, from_value, Deserializer, Value};
+pub use value_ser::to_value;
